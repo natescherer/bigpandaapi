1bdfd7cc39d43cfce6259a83c9d8ea0918cc5919
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-08-05 00:37:08 +0000

    Fix docs-build [skip ci]

--- noxfile.py
+++ noxfile.py
@@ -205,7 +205,7 @@ def xdoctest(session: Session) -> None:
     session.run("python", "-m", "xdoctest", *args)
 
 
-@session(name="docs-build", python="3.8")
+@session(name="docs-build", python="3.")
 def docs_build(session: Session) -> None:
     """Build the documentation."""
     args = session.posargs or ["docs", "docs/_build"]
